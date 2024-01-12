e9223ae8de3834ebfab60c939bbadf9e713d2907
David Roehrscheid <drwebservice@davidroehrscheid.com>
2024-01-12 22:14:59 +0900

    Change to LazyVim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,2 @@
-require('base')
-require('highlights')
-require('maps')
-require('plugins')
-
-local has = function(x)
-  return vim.fn.has(x) == 1
-end
-
-local is_mac = has "macunix"
-local is_win = has 'win32'
-
-if is_mac then
-  require('macos')
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
